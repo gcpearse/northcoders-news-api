b46959eb9570d4f6c7dff9f60b2f0b9c3b70699b
gcpearse <glen.c.pearse@gmail.com>
2023-11-20 18:22:04 +0000

    adds test for GET /api/articles

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -16,12 +16,45 @@ describe("/api", () => {
         const regex = /(GET|POST|PATCH|DELETE) \/api(\S+)?\b/;
         for (let key in body) {
           expect(key).toMatch(regex);
-          expect(body[key]).toMatchObject({
-            description: expect.any(String),
-            queries: expect.any(Array),
-            exampleResponse: expect.any(Object)
-          });
+          if (key === "GET /api") {
+            expect(body[key]).toMatchObject({
+              description: expect.any(String)
+            });
+          } else {
+            expect(body[key]).toMatchObject({
+              description: expect.any(String),
+              queries: expect.any(Array),
+              exampleResponse: expect.any(Object)
+            });
+          }
         }
+      });
+  });
+});
+
+describe("/api/articles", () => {
+  test("GET:200 responds with an array of article objects, sorted by data (created_at) in descending order", () => {
+    return request(app)
+      .get("/api/articles")
+      .expect(200)
+      .then(({ body }) => {
+        const articles = body.articles;
+        expect(articles).toHaveLength(13);
+        articles.forEach((article) => {
+          expect(article).toMatchObject({
+            article_id: expect.any(Number),
+            title: expect.any(String),
+            topic: expect.any(String),
+            author: expect.any(String),
+            created_at: expect.any(String),
+            votes: expect.any(Number),
+            article_img_url: expect.any(String),
+            comment_count: expect.any(Number)
+          });
+        });
+        expect(articles).toBeSortedBy("created_at", {
+          descending: true
+        });
       });
   });
 });
@@ -33,7 +66,6 @@ describe("/api/articles/:article_id", () => {
       .expect(200)
       .then(({ body }) => {
         const article = body.article;
-        console.log(article);
         expect(article).toMatchObject({
           article_id: 1,
           title: "Living in the shadow of a great man",
@@ -42,10 +74,11 @@ describe("/api/articles/:article_id", () => {
           body: "I find this existence challenging",
           created_at: "2020-07-09T20:11:00.000Z",
           votes: 100,
-          article_img_url: "https://images.pexels.com/photos/158651/news-newsletter-newspaper-information-158651.jpeg?w=700&h=700",
+          article_img_url: "https://images.pexels.com/photos/158651/news-newsletter-newspaper-information-158651.jpeg?w=700&h=700"
         });
       });
   });
+
   test("GET:404 responds with an error message when the article_id is valid but does not exist", () => {
     return request(app)
       .get("/api/articles/100")
@@ -54,6 +87,7 @@ describe("/api/articles/:article_id", () => {
         expect(body.message).toBe("Article not found");
       });
   });
+
   test("GET:400 responds with an error message when the article_id is invalid", () => {
     return request(app)
       .get("/api/articles/one")
