858747dd33655fc4de3ec0e5dd2d31c66cb33a5b
gcpearse <glen.c.pearse@gmail.com>
2023-11-20 14:50:02 +0000

    adds controller for /api endpoint

--- controllers/general-controllers.js
+++ controllers/general-controllers.js
@@ -1,3 +1,13 @@
+const { selectAllEndpoints } = require("../models/general-models");
+
+exports.getEndpoints = (req, res, next) => {
+  selectAllEndpoints()
+    .then((endpoints) => {
+      res.status(200).send(endpoints);
+    })
+    .catch(next);
+};
+
 exports.handleNotFound = (req, res, next) => {
   res.status(404).send({ message: "Path not found" })
     .catch(next);
