47bd23631e03475ac3a80b1942a1a170d97a6366
gcpearse <glen.c.pearse@gmail.com>
2023-11-20 14:52:20 +0000

    writes test for /api endpoint

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -25,6 +25,25 @@ describe("/api/topics", () => {
   });
 });
 
+describe("/api", () => {
+  test("GET:200 responds with an object describing all available endpoints on the API", () => {
+    return request(app)
+      .get("/api")
+      .expect(200)
+      .then(({ body }) => {
+        const regex = /(GET|POST|PATCH|DELETE) \/api(\S+)?\b/;
+        for (let key in body) {
+          expect(key).toMatch(regex);
+          expect(body[key]).toMatchObject({
+            description: expect.any(String),
+            queries: expect.any(Array),
+            exampleResponse: expect.any(Object)
+          });
+        }
+      });
+  });
+});
+
 describe("ANY /api/notapath", () => {
   test("404: responds with an error message if path not found", () => {
     return request(app)
