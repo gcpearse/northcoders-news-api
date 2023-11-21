b41a116a33d4ae91a0417e7e1b4e7da1ea15a012
gcpearse <glen.c.pearse@gmail.com>
2023-11-21 11:37:14 +0000

    adds get function for /api/articles/:article_id/comments endpoint

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 const express = require("express");
-const { getArticleById, getAllArticles } = require("./controllers/articles-controllers");
+const { getArticleById, getAllArticles, getCommentsByArticleId } = require("./controllers/articles-controllers");
 const { handleNotFound, getEndpoints } = require("./controllers/general-controllers");
 const { getTopics } = require("./controllers/topics-controllers");
 const { handleCustomErrors, handleServerErrors, handlePsqlErrors } = require("./errors");
@@ -9,6 +9,7 @@ const app = express();
 app.get("/api", getEndpoints);
 app.get("/api/articles", getAllArticles);
 app.get("/api/articles/:article_id", getArticleById);
+app.get("/api/articles/:article_id/comments", getCommentsByArticleId);
 app.get("/api/topics", getTopics);
 
 app.use(handlePsqlErrors);
