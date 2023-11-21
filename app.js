7ba41cff95d17f55569ffe5ef8464ec45a1c500b
gcpearse <glen.c.pearse@gmail.com>
2023-11-21 15:59:58 +0000

    adds middleware for post and patch requests, plus post function

--- app.js
+++ app.js
@@ -3,15 +3,18 @@ const { getArticleById, getAllArticles } = require("./controllers/articles-contr
 const { handleNotFound, getEndpoints } = require("./controllers/general-controllers");
 const { getTopics } = require("./controllers/topics-controllers");
 const { handleCustomErrors, handleServerErrors, handlePsqlErrors } = require("./errors");
-const { getCommentsByArticleId } = require("./controllers/comments-controllers");
+const { getCommentsByArticleId, postCommentByArticleId } = require("./controllers/comments-controllers");
 
 const app = express();
+app.use(express.json());
 
 app.get("/api", getEndpoints);
 app.get("/api/articles", getAllArticles);
 app.get("/api/articles/:article_id", getArticleById);
 app.get("/api/articles/:article_id/comments", getCommentsByArticleId);
 app.get("/api/topics", getTopics);
+
+app.post("/api/articles/:article_id/comments", postCommentByArticleId);
 
 app.use(handlePsqlErrors);
 app.use(handleCustomErrors);
