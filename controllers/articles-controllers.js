278ef3667b6052452bd5f6670fad30500062211b
gcpearse <glen.c.pearse@gmail.com>
2023-11-21 11:37:37 +0000

    writes controller functionality for /api/articles/:article_id/comments endpoint

--- controllers/articles-controllers.js
+++ controllers/articles-controllers.js
@@ -1,4 +1,4 @@
-const { selectArticleById, selectAllArticles } = require("../models/articles-models");
+const { selectArticleById, selectAllArticles, selectCommentsByArticleId } = require("../models/articles-models");
 
 exports.getAllArticles = (req, res, next) => {
   selectAllArticles()
@@ -13,6 +13,15 @@ exports.getArticleById = (req, res, next) => {
   selectArticleById(article_id)
     .then((article) => {
       res.status(200).send({ article });
+    })
+    .catch(next);
+};
+
+exports.getCommentsByArticleId = (req, res, next) => {
+  const { article_id } = req.params;
+  selectCommentsByArticleId(article_id)
+    .then((comments) => {
+      res.status(200).send({ comments });
     })
     .catch(next);
 };
