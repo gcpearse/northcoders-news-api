419f083b28ce5be05f7841dc154c8ac69e285bd0
gcpearse <glen.c.pearse@gmail.com>
2023-11-21 14:10:10 +0000

    refactors getCommentsByArticleId to return no comments to the user when the article ID is valid

--- controllers/comments-controllers.js
+++ controllers/comments-controllers.js
@@ -1,9 +1,15 @@
+const { checkArticleExists } = require("../models/articles-models");
 const { selectCommentsByArticleId } = require("../models/comment-models");
 
 exports.getCommentsByArticleId = (req, res, next) => {
   const { article_id } = req.params;
-  selectCommentsByArticleId(article_id)
-    .then((comments) => {
+  const commentsPromises = [selectCommentsByArticleId(article_id)];
+  if (article_id) {
+    commentsPromises.push(checkArticleExists(article_id));
+  }
+  Promise.all(commentsPromises)
+    .then((commentsPromises) => {
+      const comments = commentsPromises[0];
       res.status(200).send({ comments });
     })
     .catch(next);
