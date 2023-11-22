2230afbf3f185eb18de176dbf873fbed38d70c02
gcpearse <glen.c.pearse@gmail.com>
2023-11-22 12:10:52 +0000

    modifies getAllArticles to send a 404 status for queries yielding no results

--- controllers/articles-controllers.js
+++ controllers/articles-controllers.js
@@ -1,11 +1,19 @@
 const { selectArticleById, selectAllArticles, updateArticleById } = require("../models/articles-models");
 
+const validQueries = ["topic"]
+
 exports.getAllArticles = (req, res, next) => {
-  selectAllArticles()
+  const query = Object.keys(req.query)[0]
+  const { topic } = req.query
+  if (query && !validQueries.includes(query)) {
+    res.status(404).send({ message: "Your search did not match any results" })
+  } else {
+    selectAllArticles(topic)
     .then((articles) => {
       res.status(200).send({ articles });
     })
     .catch(next);
+  }
 };
 
 exports.getArticleById = (req, res, next) => {
