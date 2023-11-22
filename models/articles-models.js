21b8c35b69e07344ff800e3451c36aed04dab7e6
gcpearse <glen.c.pearse@gmail.com>
2023-11-22 14:27:14 +0000

    modifies selectArticleById model to include comment_count on article response object when querying

--- models/articles-models.js
+++ models/articles-models.js
@@ -46,8 +46,22 @@ exports.selectAllArticles = async (topic) => {
 
 exports.selectArticleById = (article_id) => {
   return db.query(`
-  SELECT * FROM articles
-  WHERE article_id = $1
+  SELECT
+    articles.article_id,
+    articles.title,
+    articles.topic,
+    articles.author,
+    articles.body,
+    articles.created_at,
+    articles.votes,
+    articles.article_img_url,
+    COUNT(comments.article_id)::INT
+    AS comment_count
+  FROM articles 
+  LEFT OUTER JOIN comments 
+  ON articles.article_id = comments.article_id
+  WHERE articles.article_id = $1
+  GROUP BY articles.article_id;
   `, [article_id])
     .then(({ rows }) => {
       if (!rows.length) {
