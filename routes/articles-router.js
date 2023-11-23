eb5cdf92a0d9e97d1ddb703fba84249d8d51b913
gcpearse <glen.c.pearse@gmail.com>
2023-11-23 14:32:56 +0000

    refactors to use .route() for different HTTP methods on one route

--- routes/articles-router.js
+++ routes/articles-router.js
@@ -5,8 +5,9 @@ const articlesRouter = require("express").Router();
 
 articlesRouter.get("", getArticles);
 articlesRouter.get("/:article_id", getArticleById);
-articlesRouter.get("/:article_id/comments", getCommentsByArticleId);
-articlesRouter.post("/:article_id/comments", postCommentByArticleId);
+articlesRouter.route("/:article_id/comments")
+  .get(getCommentsByArticleId)
+  .post(postCommentByArticleId);
 articlesRouter.patch("/:article_id", patchArticleById);
 
 module.exports = articlesRouter;
