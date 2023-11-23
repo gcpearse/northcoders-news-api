d6a37697ab310bfc6ee0b5751875d4ca1b257b81
gcpearse <glen.c.pearse@gmail.com>
2023-11-23 17:01:13 +0000

    updates router

--- routes/articles-router.js
+++ routes/articles-router.js
@@ -1,9 +1,11 @@
-const { getArticles, getArticleById, patchArticleById } = require("../controllers/articles-controllers");
+const { getArticles, getArticleById, patchArticleById, postArticle } = require("../controllers/articles-controllers");
 const { getCommentsByArticleId, postCommentByArticleId } = require("../controllers/comments-controllers");
 
 const articlesRouter = require("express").Router();
 
-articlesRouter.get("", getArticles);
+articlesRouter.route("")
+  .get(getArticles)
+  .post(postArticle);
 articlesRouter.get("/:article_id", getArticleById);
 articlesRouter.route("/:article_id/comments")
   .get(getCommentsByArticleId)
