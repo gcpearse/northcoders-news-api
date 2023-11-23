91caf3075c74412c30fae9b889d7953d387aa144
gcpearse <glen.c.pearse@gmail.com>
2023-11-23 14:59:57 +0000

    adds controller for GET requests on the /api/users/:username endpoint and fixes missing catch block on getUsers function

--- controllers/users-controllers.js
+++ controllers/users-controllers.js
@@ -1,8 +1,18 @@
-const { selectAllUsers } = require("../models/users-models");
+const { selectAllUsers, selectUserByUsername } = require("../models/users-models");
 
 exports.getUsers = (req, res, next) => {
   selectAllUsers()
     .then((users) => {
       res.status(200).send({ users });
-    });
+    })
+    .catch(next);
+};
+
+exports.getUserByUsername = (req, res, next) => {
+  const { username } = req.params;
+  selectUserByUsername(username)
+    .then((user) => {
+      res.status(200).send({ user });
+    })
+    .catch(next);
 };
